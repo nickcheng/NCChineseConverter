95aed92b22a490e4940a6f2936a7585d5a996455
nickcheng <n@nickcheng.com>
2013-03-04 15:34:14 +0800

    Example

---
+++ Example/ChineseConverter Example/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ChineseConverter Example
+//
+//  Created by nickcheng on 13-2-28.
+//  Copyright (c) 2013年 NC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/ChineseConverter Example/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ChineseConverter Example
+//
+//  Created by nickcheng on 13-2-28.
+//  Copyright (c) 2013年 NC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
